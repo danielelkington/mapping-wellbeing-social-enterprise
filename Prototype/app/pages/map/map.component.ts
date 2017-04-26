eac6d5031ab9d94a2e53de79b50a2d03eafdb92a
saicheongkam <100614773@student.swin.edu.au>
2017-04-26 15:46:55 +1000

    Set Map Icons
    Setup a Map view with some various icons that display a simple message
    when tapped.

--- Prototype/app/pages/map/map.component.ts
+++ Prototype/app/pages/map/map.component.ts
@@ -10,4 +10,55 @@ import { Config } from "../../config";
 })
 export class MapComponent{
     mapboxAccessToken = Config.mapboxAccessToken;
+
+    private map: any
+
+  onMapReady(args)
+  {
+    this.map = args.map;
+    args.map.addMarkers([
+        {
+          lat: -37.8223,
+          lng: 145.03784,
+          title: 'Swinburne EN',
+          subtitle: 'The Engineering Building',
+          icon: 'http://i.imgur.com/cdFPldG.png'
+        },
+        {
+          lat: -37.8228,
+          lng: 145.03842,
+          title: 'Swinburne ATC',
+          subtitle: 'The Advanced Technologies Centre',
+          icon: 'http://i.imgur.com/SRuzpbn.png'
+        },
+        {
+          lat: -37.8229,
+          lng: 145.03925,
+          title: 'Swinburne AMDC',
+          subtitle: 'The Advanced Manufacturing and Design Centre',
+          icon: 'http://i.imgur.com/C4pv4eZ.png'
+        },
+        {
+          lat: -37.8225,
+          lng: 145.03935,
+          title: 'Swinburne LB',
+          subtitle: 'The Swinburne Library',
+          icon: 'http://i.imgur.com/eInZqUj.png'
+        },
+        {
+          lat: -37.8221,
+          lng: 145.03940,
+          title: 'Swinburne BA',
+          subtitle: 'The Business and Arts Building',
+          icon: 'http://i.imgur.com/92R3pMP.png'
+        },
+        {
+          lat: -37.8214,
+          lng: 145.03846,
+          title: 'Swinburne GS',
+          subtitle: 'The George Swinburne Building',
+          icon: 'http://i.imgur.com/mJKaUem.png'
+        }]
+      );
+  }
 }
