f429a1f68b93ad9c345b04cc12afd3d71866a5d0
saicheongkam <100614773@student.swin.edu.au>
2017-08-28 16:37:53 +1000

    Navigating and Displaying Map with Places
    Navigation to map view which displays the selected participants' places.

--- Frontend/app/app.routing.ts
+++ Frontend/app/app.routing.ts
@@ -1,13 +1,16 @@
 import { EnterprisesComponent } from "./pages/enterprises/enterprises.component";
 import { ParticipantsComponent } from "./pages/participants/participants.component";
+import { MapComponent } from "./pages/map/map.component";
 
 export const routes = [
     { path: "", redirectTo: "/enterprises", pathMatch: "full"},
     { path: "enterprises", component: EnterprisesComponent },
-    { path: "participants/:id", component: ParticipantsComponent }
+    { path: "participants/:id", component: ParticipantsComponent },
+    { path: "map/:eId/:pId", component: MapComponent }
 ];
 
 export const navigatableComponents = [
     EnterprisesComponent,
-    ParticipantsComponent
+    ParticipantsComponent,
+    MapComponent
 ];
