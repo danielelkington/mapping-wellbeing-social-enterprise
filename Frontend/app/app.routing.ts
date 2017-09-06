edfbe647af9bcaf9b45c2d984b2b18df9ec24350
Daniel Elkington <danielelkington@me.com>
2017-09-06 16:33:29 +1000

    Fixing info screen to display when button pressed

--- Frontend/app/app.routing.ts
+++ Frontend/app/app.routing.ts
@@ -2,9 +2,11 @@ import { EnterprisesComponent } from "./pages/enterprises/enterprises.component"
 import { ParticipantsComponent } from "./pages/participants/participants.component";
 import { MapComponent } from "./pages/map/map.component";
 import { PlaceComponent } from "./pages/place/place.component";
+import { InfoComponent } from "./pages/info/info.component";
 
 export const routes = [
-    { path: "", redirectTo: "/enterprises", pathMatch: "full"},
+    { path: "", redirectTo: "/enterprises", pathMatch: "full" },
+    { path: "info", component: InfoComponent },
     { path: "enterprises", component: EnterprisesComponent },
     { path: "participants/:id", component: ParticipantsComponent },
     { path: "map/:eId/:pId", component: MapComponent },
@@ -15,5 +17,6 @@ export const navigatableComponents = [
     EnterprisesComponent,
     ParticipantsComponent,
     MapComponent,
-    PlaceComponent
+    PlaceComponent,
+    InfoComponent
 ];
